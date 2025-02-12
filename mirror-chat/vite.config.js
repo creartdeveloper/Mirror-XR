824f33fbd49c9ea08312b860c2ca4e06554fbcad
Nur988 <proteas988@gmail.com>
2025-02-12 16:04:12 +0600

    test

--- mirror-chat/vite.config.js
+++ mirror-chat/vite.config.js
@@ -3,5 +3,11 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   base: '/mirror-chat/',
-  plugins: [react()]
+  plugins: [react()],
+  resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx']
+  },
+  server: {
+    open: true
+  }
 })
